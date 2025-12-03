72913a1a74216ffdd9075dda8fbe60f6baa08753
Ozair-khaan <ozairkhantech@gmail.com>
2025-12-03 17:55:49 +0530

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  // 👇 WE ARE SWITCHING BACK TO FILE LINKING
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // 1. The State (Data)
   myName = 'Ozair';
+  skills = ['HTML', 'CSS', 'JavaScript', 'jQuery'];
 
-  // 2. The Action (Function)
   changeName() {
     this.myName = 'Angular Expert';
+  }
+
+  addSkill() {
+    this.skills.push('Angular v17');
   }
 }
